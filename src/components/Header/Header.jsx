5f40f36207da2158dbab4d77d90efbccb47844b5
annadan1 <anna_s92_@mail.ru>
2023-07-28 14:02:06 +0400

    Change scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,9 +6,13 @@ import { ImagesBlock } from "./ImagesBlock/ImagesBlock";
 export const Header = () => {
   return (
     <header className={styles.container}>
-      <h1>Knightsbridge private park – элитный район Лондона в центре</h1>
-      <div className={styles.container__background}>
-        <div className={styles.container__background__content}>
+      <div className={styles.container__background}></div>
+      <div className={styles.container__content}>
+        <h1>
+          Knightsbridge private park – элитный район Лондона в центре{" "}
+          <span className={styles.container__1440hidden}>Москвы</span>
+        </h1>
+        <div className={styles.container__content__about}>
           <AboutUsInNumbers />
           <AboutUs />
         </div>
